05875628609642b16fc90659973f460b40f911e0
lucas <lucas@pop-os.localdomain>
2024-05-15 22:55:56 -0300

    models owner

--- contact/admin.py
+++ contact/admin.py
@@ -3,6 +3,16 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    print('oi')
+    list_display =  'id','first_name', 'last_name', 'phone'
+    ordering = 'id',
+    search_fields = 'id','first_Name', 'last_name'
+    list_per_page =1
+    list_max_show_all = 200 
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
 
 # Register your models here.
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+    name = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.name
 
 # Create your models here.
 class Contact(models.Model):
@@ -9,7 +17,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_Date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
-
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True) 
+    owner =  models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True) 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
 
