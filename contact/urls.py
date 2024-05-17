305b20007e73a646ffa7d207fef97ab3156d1919
lucas <lucas@pop-os.localdomain>
2024-05-16 21:38:30 -0300

    new contacts

--- contact/urls.py
+++ contact/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from contact import views
 app_name = 'contact'
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
 
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django.http import Http404
 from contact.models import Category
+from django.shortcuts import get_object_or_404, render
+
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.all().filter(show=True).order_by('id')[:10]
     context = {
         'contacts': contacts
     }
     return render(request,'contact/index.html', context )
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    # if single_contact is None:
+    #     raise Http404()
+    single_contact = get_object_or_404(Contact.objects, pk=contact_id)
+    context = {
+        'contact': single_contact
+    }
+    return render(request,'contact/contact.html', context )
 # Create your views here.
