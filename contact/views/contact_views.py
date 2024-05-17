56277c7c9c063459f667397d804791cdccbe897f
lucas <lucas@pop-os.org.local>
2024-05-17 16:22:43 -0300

    contact views

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+
 from contact.models import Contact
-from django.http import Http404
-from contact.models import Category
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404,redirect,render
 
 def index(request):
-    contacts = Contact.objects.all().filter(show=True).order_by('id')[:10]
+    contacts = Contact.objects.all().filter(show=True).order_by('id')
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title':'Contatos - '
+    }
+    return render(request,'contact/index.html', context )
+
+def search(request):
+  
+    search_value = request.GET.get('q','').strip() 
+    if search_value == '':
+        return redirect('contact:index')
+        
+    print('search_value',search_value)
+    contacts = Contact.objects.filter(show=True).filter(first_name__icontains=search_value).order_by('id')
+    context = {
+        'contacts': contacts,
+        'site_title':'Search - '
     }
     return render(request,'contact/index.html', context )
 def contact(request, contact_id):
@@ -15,8 +29,10 @@ def contact(request, contact_id):
     # if single_contact is None:
     #     raise Http404()
     single_contact = get_object_or_404(Contact.objects, pk=contact_id)
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
     return render(request,'contact/contact.html', context )
 # Create your views here.
