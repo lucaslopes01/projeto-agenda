5e0042b73f417401ce57c656c427f8f9709ae089
lucas <lucas@pop-os.org.local>
2024-05-22 13:39:59 -0300

    atualizações

--- contact/urls.py
+++ contact/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
     path('contact/<int:contact_id>/detail', views.contact, name='contact'),# type:ignore
     path('contact/create/', views.create, name='create'),# type:ignore
     path('contact/<int:contact_id>/update/', views.update, name='update'),# type:ignore
-    path('contact/<int:contact_id>/delete/', views.update, name='delete'),# type:ignore
+    path('contact/<int:contact_id>/delete/', views.update, name='delete '),# type:ignore
+
+    path('user/create/', views.register, name='resgister'),# type:ignore
+    path('user/login/', views.login_view, name='login'),# type:ignore
+    path('user/logout/', views.logout_view, name='logout'),# type:ignore
+    path('user/update/', views.user_update, name='user_update'),# type:ignore
+
 
 
 ]
